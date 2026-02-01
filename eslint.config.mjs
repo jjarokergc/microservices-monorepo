d4cfa947f91169bb080cb3c9fd9657fc3e849ae2
Jon Jaroker <git@jaroker.com>
2026-02-01 14:39:25 -0500

    Replace biome with prettier-eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// eslint.config.mjs
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig, // turns off formatting rules
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error", // ← makes Prettier violations ESLint errors
+      // add your own rules here
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      // ...
+    },
+  },
+);
