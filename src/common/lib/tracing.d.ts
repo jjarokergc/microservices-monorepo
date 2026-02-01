00d86ba6a2c7c50d5d377c5a033682bf7ea5e6c5
Jon Jaroker <git@jaroker.com>
2026-02-01 15:12:07 -0500

    set up openteletry

---
+++ src/common/lib/tracing.d.ts
@@ -0,0 +1,11 @@
+declare module "@/common/lib/tracing.js" {
+	import type { Tracer } from "@opentelemetry/api";
+
+	/**
+	 * Initialize OpenTelemetry tracing for a service
+	 * and return a tracer instance.
+	 */
+	function tracing(serviceName: string): Tracer;
+
+	export = tracing;
+}
